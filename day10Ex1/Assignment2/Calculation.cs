cc7acd462139c2b65f6533d5f1d686afbe2e1432
xBRAZENx <87162097+xBRAZENx@users.noreply.github.com>
2023-07-21 16:41:39 +0530

    Add files via upload

---
+++ day10Ex1/Assignment2/Calculation.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    public class Calculation
+    {
+        int a, b, result;
+        public int Add(int a, int b)
+        { return a + b; }
+        public int Subtract(int a, int b) {  return a - b; }
+        public int Multiply(int a, int b)
+        {
+                return a * b;
+        }
+        public int Divide(int a, int b)
+        {
+            return a / b;
+        }
+        public bool IsPrime(int a)
+        {
+            if (a % 2 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                if (a == 2)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }   }
+    }
+}
---
+++ day10Ex1/Assignment2/Tests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Assignment2
+{
+    [TestFixture]
+    public class Tests
+    {
+        Calculation c = new Calculation();
+        [Test]
+        public void testadd()
+        {
+            int a= c.Add(5, 3);
+            Assert.AreEqual(8, a);
+
+        }
+        [Test]
+        public void testsub()
+        {
+            int a =c.Subtract(5, 3);
+            Assert.AreEqual(2, a);
+
+        }
+        [Test]
+        public void testmultiply()
+        {
+            int a=c.Multiply(5, 3);
+            Assert.AreEqual(15, a);
+        }
+        [Test]
+        public void testdivide()
+        {
+            int a= c.Divide(15,3);
+            Assert.AreEqual(5, a);
+        }
+        [Test]
+        public void testprime()
+        {
+            bool s = c.IsPrime(2);
+            Assert.AreEqual(true, s);
+        }
+
+    }
+}
